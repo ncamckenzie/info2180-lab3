96bae3eaba3157f9056a172568fd6677db07869e
Nicholas McKenzie <ncamckenzie@gmail.com>
2020-10-10 22:23:35 -0500

    Exercise 2 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,24 @@ window.onload = function() {
     var i;
     var squares = document.querySelectorAll("#board div");
     for (i=0;i<=9;i++){
-    squares[i].classList.add("square");
+    squares[i].classList.add("square",i);
+    squares[i].addEventListener("click",fillSqaure);
+    
 }
 }
+let gameprogress = [];
+
+function fillSqaure(x){
+    const clickedSquare = x.target.classList[1];
+    if (gameprogress.length < 9 && gameprogress.length % 2 == 0){
+    document.querySelectorAll("#board div")[clickedSquare].innerHTML = "X";
+    document.querySelectorAll("#board div")[clickedSquare].classList.add("square", "X", clickedSquare);
+    gameprogress.push("X");
+
+    }
+   else if (gameprogress.length < 9 && gameprogress.length % 2 != 0){
+    document.querySelectorAll("#board div")[clickedSquare].innerHTML = "O";
+    document.querySelectorAll("#board div")[clickedSquare].classList.add("square","O", clickedSquare);
+    gameprogress.push("O");
+    
+   }}
