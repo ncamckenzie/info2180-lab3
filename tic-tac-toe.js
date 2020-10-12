f97550b7b9bc30fe963a5fa2316100ee953654ca
Nicholas McKenzie <ncamckenzie@gmail.com>
2020-10-11 19:18:48 -0500

    Exercise 4 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,10 +6,10 @@ window.onload = function() {
     squares[i].addEventListener("click",fillSqaure);
     squares[i].addEventListener("mouseover",changeSquare);
     squares[i].addEventListener("mouseout",changeSquareBack);
-    
-    
+
 }
 }
+
 let gameprogress = [];
 
 function fillSqaure(x){
@@ -25,7 +25,9 @@ function fillSqaure(x){
     document.querySelectorAll("#board div")[clickedSquare].classList.add("square","O", clickedSquare);
     gameprogress.push("O");
     
-   }}
+   }
+   gameWinner();
+}
 
    function changeSquare(x) {
        const hoveredSquare = x.target.classList[1];
@@ -39,3 +41,43 @@ function fillSqaure(x){
         document.querySelectorAll("#board div")[unhoveredSquare].classList.remove(x[2]);
         document.querySelectorAll("#board div")[unhoveredSquare].classList.add("hover O");
    }}
+
+const winningPattern = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+
+function gameWinner(){
+    var sqaureList = document.querySelectorAll("#board div");
+    if (sqaureList[0].className.includes("X") && sqaureList[1].className.includes("X") && sqaureList[2].className.includes("X")
+    ||sqaureList[3].className.includes("X") && sqaureList[4].className.includes("X") && sqaureList[5].className.includes("X") 
+    || sqaureList[6].className.includes("X") && sqaureList[7].className.includes("X") && sqaureList[8].className.includes("X") 
+    || sqaureList[0].className.includes("X") && sqaureList[3].className.includes("X") && sqaureList[6].className.includes("X")
+    ||sqaureList[1].className.includes("X") && sqaureList[4].className.includes("X") && sqaureList[7].className.includes("X") ||
+    sqaureList[2].className.includes("X") && sqaureList[5].className.includes("X") && sqaureList[8].className.includes("X") ||
+    sqaureList[0].className.includes("X") && sqaureList[4].className.includes("X") && sqaureList[8].className.includes("X") ||
+    sqaureList[2].className.includes("X") && sqaureList[4].className.includes("X") && sqaureList[6].className.includes("X")){
+     document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+     document.getElementById("status").classList.add("you-won");}
+    else if(sqaureList[0].className.includes("O") && sqaureList[1].className.includes("O") && sqaureList[2].className.includes("O")
+    ||sqaureList[3].className.includes("O") && sqaureList[4].className.includes("O") && sqaureList[5].className.includes("O") 
+    || sqaureList[6].className.includes("O") && sqaureList[7].className.includes("O") && sqaureList[8].className.includes("O") 
+    || sqaureList[0].className.includes("O") && sqaureList[3].className.includes("O") && sqaureList[6].className.includes("O")
+    ||sqaureList[1].className.includes("O") && sqaureList[4].className.includes("O") && sqaureList[7].className.includes("O") ||
+    sqaureList[2].className.includes("O") && sqaureList[5].className.includes("O") && sqaureList[8].className.includes("O") ||
+    sqaureList[0].className.includes("O") && sqaureList[4].className.includes("O") && sqaureList[8].className.includes("O") ||
+    sqaureList[2].className.includes("O") && sqaureList[4].className.includes("O") && sqaureList[6].className.includes("O")){
+     document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+     document.getElementById("status").classList.add("you-won");
+    }
+    }
+
+
